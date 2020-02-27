9a912a86d85ece4a40fe92e9deac454afdaf8fa0
Jakub Kala <kalajakub96@gmail.com>
2020-02-27 21:58:40 +0100

    TODO transpose

--- problem-set-1/neuralnetwork.py
+++ problem-set-1/neuralnetwork.py
@@ -1,71 +1,104 @@
 import nn_basic_functions as func
 import numpy as np
 from matplotlib import pyplot as plt
+import random
 
+class Layer:
+    def __init__(self, n_of_neurons: int, n_of_neurons_prev, activation_function_and_derivative):
+        self.activation_function, self.activation_function_derivative = func.af_text2func(activation_function_and_derivative)
+        self.weights = func.initialize_weights(n_of_neurons, n_of_neurons_prev)
+        return
+
+    def propagate(self, A):
+        Z = np.dot(self.weights['W'], A) + self.weights['b']
+        A = self.activation_function(Z)
+        return A, Z
+
+    def back_propagate(self, dA, Z, A_prev):
+        dZ = dA * self.activation_function_derivative(Z)
+        dW = np.dot(dZ, np.transpose(A_prev))
+        db = np.sum(dZ, axis=1, keepdims=True)
+        dA_prev = np.dot(np.transpose(self.weights['W']), dZ)
+        return dW, db, dA_prev
+
+    def update_weights(self, dW, db, learning_rate):
+        self.weights['W'] = self.weights['W'] - learning_rate * dW
+        self.weights['b'] = self.weights['b'] - learning_rate * db
+        return
 
 class NeuralNetworkClassifier:
-    class Layer:
-        def __init__(self, m, n_of_neurons: int, n_of_neurons_prev, activation_function_and_derivative):
-            self.m = m
-            self.activation_function, self.activation_function_derivative = activation_function_and_derivative
-            self.weights = func.initialize_weights(n_of_neurons, n_of_neurons_prev)
+    #TODO: what about regression?
 
-        def propagate(self, A):
-            Z = np.dot(self.weights['W'], A) + self.weights['b']
-            A = self.activation_function(Z)
-            return A, Z
+    def __init__(self, learning_rate=1e-3, batch_size=1):
 
-        def back_propagate(self, dA, Z, A_prev):
-            dZ = dA * self.activation_function_derivative(Z)
-            dW = (1 / self.m) * np.dot(dZ, np.transpose(A_prev))
-            db = (1 / self.m) * np.sum(dZ, axis=1, keepdims=True)
-            dA_prev = np.dot(np.transpose(self.weights['W']), dZ)
-            return dW, db, dA_prev
-
-        def update_weights(self, dW, db, learning_rate):
-            self.weights['W'] = self.weights['W'] - learning_rate * dW
-            self.weights['b'] = self.weights['b'] - learning_rate * db
-
-    def __init__(self, X_train, y_train, neuron_numbers: list, activation_functions: list, learning_rate):
-        self.n, self.m = X_train.shape
-        self.X_train, self.y_train = X_train, y_train
         self.learning_rate = learning_rate
+        self.batch_size = batch_size
         self.cache = {}
-        self.layers = [self.Layer(self.m, neuron_numbers[0], self.n, func.af_text2func(activation_functions[0]))]
-        self.loss_on_iteration = None
-        for i in range(1, len(neuron_numbers)):
-            self.layers.append(self.Layer(self.m, neuron_numbers[i], neuron_numbers[i - 1],
-                                          func.af_text2func(activation_functions[i])))
+        self.layers = []
+        #self.layers = [self.Layer(self.m, neuron_numbers[0], self.n, func.af_text2func(activation_functions[0]))]
+        return
 
-    def train(self, iterations):
-        loss_on_iteration = []
-        for iteration in range(iterations):
-            A = self.X_train
-            self.cache['A0'] = A
-            for i in range(len(self.layers)):
-                A, Z = self.layers[i].propagate(A)
-                self.cache['Z' + str(i + 1)] = Z
-                self.cache['A' + str(i + 1)] = A
-            loss_on_iteration.append(func.loss_function(A, self.y_train, self.m))
+    def add_layer(self, layer):
+        #TODO: bias trigger
+        assert isinstance(layer, Layer), "Input is not a nn layer!"
+        self.layers.append(layer)
 
-            dA = - (np.divide(self.y_train, A) - np.divide(1 - self.y_train, 1 - A))
+    def train(self, X, y, epochs):
+        loss_on_epoch = []
 
-            for i in range(len(self.layers) - 1, -1, -1):
-                Z = self.cache['Z' + str(i + 1)]
-                A_prev = self.cache['A' + str(i)]
-                dW, db, dA_prev = self.layers[i].back_propagate(dA, Z, A_prev)
-                self.layers[i].update_weights(dW, db, self.learning_rate)
-                dA = dA_prev
+        def _create_batches(n_obs, batch_size):
+            idxs = [i for i in range(n_obs)]
+            batches = []
+            while len(idxs) > batch_size:
+                batch = random.sample(idxs, batch_size)
+                batches.append(batch)
+                for i in batch: idxs.remove(i)
+            if len(idxs) != 0: batches.append(idxs)
+            return batches
+        batches = _create_batches(X.shape[0], self.batch_size)
 
-            if iteration % 100 == 0:
-                print(f'Cost after {iteration} iteration: {loss_on_iteration[iteration]}')
-        self.loss_on_iteration = loss_on_iteration
+        for epoch in range(epochs):
+
+            for batch in batches:
+                X_train = X[batch, :]
+                y_train = y[:, batch]
+                # propagation
+
+                self.cache['A0'] = X_train
+                A = X_train
+                for i in range(len(self.layers)):
+                    A, Z = self.layers[i].propagate(A)
+                    self.cache['Z' + str(i + 1)] = Z
+                    self.cache['A' + str(i + 1)] = A
+                loss_on_iteration.append(func.loss_function(A, y_train, self.batch_size))
+
+                # TODO: it should work for regression tasks also;
+                dA = - (np.divide(y_train, A) - np.divide(1 - y_train, 1 - A))
+
+                # back propagation
+                for i in range(len(self.layers) - 1, -1, -1):
+                    Z = self.cache['Z' + str(i + 1)]
+                    A_prev = self.cache['A' + str(i)]
+                    dW, db, dA_prev = self.layers[i].back_propagate(dA, Z, A_prev)
+
+                    #
+                    dW /= (1/self.batch_size)
+                    db /= (1/self.batch_size)
+
+                    self.layers[i].update_weights(dW, db, self.learning_rate)
+                    dA = dA_prev
+
+            if epoch % 100 == 0:
+                print(f'Cost after {epoch} epoch: {loss_on_epoch[epoch]}')
+
+            #???
+            self.loss_on_epoch = loss_on_epoch
 
     def plot_loss(self):
-        iterations = len(self.loss_on_iteration)
+        epochs = len(self.loss_on_epoch)
         fig = plt.figure(figsize=(12, 6))
         ax1 = fig.add_subplot(111)
-        ax1.plot(list(range(iterations)), self.loss_on_iteration)
+        ax1.plot(list(range(epochs)), self.loss_on_epoch)
         plt.show()
 
 
@@ -77,3 +110,9 @@ def normalize_data(X_train, X_test):
     X_train = (X_train - mi) / sigma
     X_test = (X_test - mi) / sigma
     return X_train, X_test
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
